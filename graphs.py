ae7f7255d1ea540a32b44b69d07cb8eceaa13000
deafcat <nikos_protopapas@hotmail.com>
2017-06-28 01:25:58 +0100

    tuesday night last

--- graphs.py
+++ graphs.py
@@ -1,1 +1,11 @@
 import networkx as nx
+
+G= nx.DiGraph()
+G.add_nodes_from([1,2,3])
+print(G.nodes())
+G.add_edge(1,2)
+G.add_edge(2,1)
+print(G.edges())
+nx.set_node_attributes(G,'winner',{1: True, 2: False, 3:False})
+print(G.nodes())
+print(G.node[1]['winner'])
